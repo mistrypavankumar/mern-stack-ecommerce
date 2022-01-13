dfc852f6a34257f73a37ac1fdac60f070f21364f
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-01-13 21:43:17 +0530

    kept all routes in a folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,15 @@
-import { Route, Routes, Navigate } from "react-router-dom";
 import Navbar from "./components/layout/Navbar/Navbar";
-import HomePage from "./pages/HomePage";
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
 import { useEffect } from "react";
 import WebFont from "webfontloader";
 import Footer from "./components/layout/Footer/Footer";
 import footerData from "./data/footerData.json";
 import "./App.css";
-import PageNotFound from "./pages/PageNotFound";
-import LoginSignUp from "./pages/User/LoginSignUp";
 
 import store from "./store";
-import { useSelector } from "react-redux";
-import { loadUser } from "./actions/userAction";
-import Profile from "./pages/User/Profile";
-import UpdateProfile from "./pages/User/UpdateProfile";
-import UpdatePassword from "./pages/User/UpdatePassword";
-import ForgotPassword from "./pages/User/ForgotPassword";
-import ResetPassword from "./pages/User/ResetPassword";
 
-import ProductDetails from "./pages/Product/ProductDetails";
-import Products from "./pages/Product/Products";
-import SearchProducts from "./pages/Product/SearchProducts";
-import Cart from "./pages/Cart/Cart";
-import Shipping from "./pages/Cart/Shipping";
+import { loadUser } from "./actions/userAction";
+
+import ElementWithRoutes from "./routes/ElementWithRoutes";
 
 const menuOptions = [
   {
@@ -46,8 +31,6 @@ const menuOptions = [
 ];
 
 function App() {
-  const { isAuthenticated } = useSelector((state) => state.user);
-
   useEffect(() => {
     WebFont.load({
       google: {
@@ -65,52 +48,7 @@ function App() {
       <Navbar webName="E-Commerce" menuOptions={menuOptions} />
 
       {/* All routes */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:keyword" element={<Products />} />
-
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/contactus" element={<ContactUs />} />
-        <Route path="/search" element={<SearchProducts />} />
-
-        {/* Protected routes starts */}
-
-        <Route
-          path="/account"
-          element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/update"
-          element={
-            isAuthenticated ? <UpdateProfile /> : <Navigate to="/login" />
-          }
-        />
-        <Route
-          path="/password/update"
-          element={
-            isAuthenticated ? <UpdatePassword /> : <Navigate to="/login" />
-          }
-        />
-
-        <Route
-          path="/shipping"
-          element={isAuthenticated ? <Shipping /> : <Navigate to="/login" />}
-        />
-
-        {/* Protected routes ends */}
-
-        <Route path="/cart" element={<Cart />} />
-
-        <Route path="/login" element={<LoginSignUp />} />
-        <Route path="/password/forgot" element={<ForgotPassword />} />
-        <Route path="/password/reset/:token" element={<ResetPassword />} />
-
-        {/* If router is not specified then show below page */}
-        <Route path="/*" element={<PageNotFound />} />
-      </Routes>
+      <ElementWithRoutes />
 
       {/* Footer component */}
       <Footer jsonData={footerData} />
---
+++ frontend/src/routes/ElementWithRoutes.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import AboutUs from "../pages/AboutUs";
+import Cart from "../pages/Cart/Cart";
+import Shipping from "../pages/Cart/Shipping";
+import ContactUs from "../pages/ContactUs";
+import HomePage from "../pages/HomePage";
+import PageNotFound from "../pages/PageNotFound";
+import ProductDetails from "../pages/Product/ProductDetails";
+import Products from "../pages/Product/Products";
+import SearchProducts from "../pages/Product/SearchProducts";
+import ForgotPassword from "../pages/User/ForgotPassword";
+import LoginSignUp from "../pages/User/LoginSignUp";
+import Profile from "../pages/User/Profile";
+import ResetPassword from "../pages/User/ResetPassword";
+import UpdatePassword from "../pages/User/UpdatePassword";
+import UpdateProfile from "../pages/User/UpdateProfile";
+
+const ElementWithRoutes = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:keyword" element={<Products />} />
+
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/search" element={<SearchProducts />} />
+
+        {/* Protected routes starts */}
+
+        <Route
+          path="/account"
+          element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/update"
+          element={
+            isAuthenticated ? <UpdateProfile /> : <Navigate to="/login" />
+          }
+        />
+        <Route
+          path="/password/update"
+          element={
+            isAuthenticated ? <UpdatePassword /> : <Navigate to="/login" />
+          }
+        />
+
+        <Route
+          path="/shipping"
+          element={isAuthenticated ? <Shipping /> : <Navigate to="/login" />}
+        />
+
+        {/* Protected routes ends */}
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/login" element={<LoginSignUp />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+
+        {/* If router is not specified then show below page */}
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default ElementWithRoutes;
