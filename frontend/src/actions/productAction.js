0bb21e18fe7e1a3686cc952dbe9afd3b0140698b
mg143pavankumar <pavansharma.mg143@gmail.com>
2022-01-15 21:22:46 +0530

    added add review dialog

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -8,6 +8,9 @@ import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
+  NEW_REVIEW_FAIL,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_REQUEST,
 } from "../constants/productConstants";
 
 // fetching all products
@@ -57,6 +60,29 @@ export const getProductDetails = (id) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_DETAILS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// creating new review
+export const newReview = (reviewData) => async (dispatch) => {
+  try {
+    dispatch({ type: NEW_REVIEW_REQUEST });
+
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const { data } = await axios.put(`/api/v1/review`, reviewData, config);
+
+    dispatch({
+      type: NEW_REVIEW_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: NEW_REVIEW_FAIL,
       payload: error.response.data.message,
     });
   }
